9d3ec3fbb50f54846b54dad348adc916bacf0240
Arif <dewisandrax@gmail.com>
2017-11-20 19:13:00 -0800

    Update maps.js

--- www/js/maps.js
+++ www/js/maps.js
@@ -15,6 +15,28 @@
         map: map,
         key: snapshot.key()
       });
+        
+        
+        
+        
+        
+         var map;
+    document.addEventListener("deviceready", function() {
+      var div = document.getElementById("map");
+      // Initialize the map view
+      map = plugin.google.maps.Map.getMap(div);
+      // Wait until the map is ready status.
+      map.addEventListener(plugin.google.maps.event.MAP_READY, onMapReady);
+    }, false);
+    function onMapReady() {
+      var button = document.getElementById("button");
+      button.addEventListener("click", onBtnClicked, false);
+    }
+    function onBtnClicked() {
+      map.showDialog();
+    }
+        
+        
 
            
 
